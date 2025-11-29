4fcc764492c498c3c98fefafaa2454fdcf170a04
Ivan-wdx <ivanbacani57@gmail.com>
2025-11-29 23:01:47 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Car Dealer PHP Activity</title>
+    <link rel = "stylesheet" href = "css/styles.css">
+</head>
+<body>
+
+<h1>Car Dealership System</h1>
+<hr>
