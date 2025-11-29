64052bbd493905b581714fb507dbf63d53ecbc06
Ivan-wdx <ivanbacani57@gmail.com>
2025-11-29 23:02:05 +0800

    Create data.php

---
+++ data.php
@@ -0,0 +1,9 @@
+<?php
+$cars = [
+    ["model" => "Toyota Vios", "price" => 500000],
+    ["model" => "Honda Civic", "price" => 1200000],
+    ["model" => "BMW X5", "price" => 4500000],
+    ["model" => "Suzuki Swift", "price" => 650000],
+    ["model" => "Ford Ranger", "price" => 1500000]
+];
+?>
