5b2f9563186a8b420795043dfdbc5f74cfb558be
Ivan-wdx <ivanbacani57@gmail.com>
2025-12-04 10:23:24 +0800

    Update data.php

--- data.php
+++ data.php
@@ -1,9 +1,9 @@
 <?php
 $cars = [
-    ["model" => "Toyota Vios", "price" => 500000],
-    ["model" => "Honda Civic", "price" => 1200000],
-    ["model" => "BMW X5", "price" => 4500000],
-    ["model" => "Suzuki Swift", "price" => 650000],
-    ["model" => "Ford Ranger", "price" => 1500000]
+    ["model" => "Toyota Supra MK4", "price" => 1200000],
+    ["model" => "Nissan Skyline R34 GT-R", "price" => 2100000],
+    ["model" => "Mazda RX-7 FD", "price" => 1001000],  
+    ["model" => "Mitsubishi Lancer Evo X", "price" => 1120000],
+    ["model" => "Subaru Impreza WRX STI", "price" => 1425000]
 ];
 ?>
